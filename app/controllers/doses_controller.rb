c81edcf4cd903408538f073f190dff2577b5a364
Friso Jelsma <friso.jelsma@gmail.com>
2017-03-23 18:08:37 +0100

    doses not done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,23 @@
 class DosesController < ApplicationController
+before_action :set_cocktail
+
   def new
+    @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@dose)
+    else
+      render :new
+    end
   end
+
+private
 
   def destroy
   end
+
+  # @cocktail = Cocktail.find(params[:cocktail_id])
 end
