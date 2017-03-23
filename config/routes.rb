7fd54932cc2f764ac6e96369edf47b7b25602bd5
Friso Jelsma <friso.jelsma@gmail.com>
2017-03-23 15:22:18 +0100

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :restaurants
-# root 'articles#index'
+  get 'doses/new'
 
+  get 'doses/create'
+
+  get 'doses/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
 end
 
