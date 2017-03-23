f9067b0b12577c0a874c25f8d21d431c63ab34d4
Friso Jelsma <friso.jelsma@gmail.com>
2017-03-23 14:43:47 +0100

    added resources restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Orange")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name:"Gin")
+Ingredient.create(name:"Dark rum")
+Ingredient.create(name:"Scotch")
+Ingredient.create(name:"Lemon vodka")
+Ingredient.create(name:"Amaretto")
+Ingredient.create(name:"Rum")
+Ingredient.create(name:"Vodka")
+Ingredient.create(name:"Tequila")
+Ingredient.create(name:"Cognac")
+Ingredient.create(name:"Salt")
+Ingredient.create(name:"Passion fruit juice")
+Ingredient.create(name:"Whiskey")
+
